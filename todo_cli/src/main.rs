a8d2ef057c0981eb6ba398ebbb55634453043086
Aadil Mehmood Khan <adilkhan11@gmail.com>
2025-11-11 22:17:29 -0500

    Implemented a simple cli

--- todo_cli/src/main.rs
+++ todo_cli/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use clap::{Parser, Subcommand};
+use todo_lib::{FileStorage, ToDo};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    operation: Operation,
+}
+
+#[derive(Subcommand)]
+enum Operation {
+    List,
+    Add { name: String },
+    Delete { id: u32 },
+    Complete { id: u32 },
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    let storage = FileStorage::new("todo.json")?;
+    let mut todo = ToDo::new(storage)?;
+
+    match cli.operation {
+        Operation::List => {
+            println!("{:#?}", todo.list_tasks());
+            return Ok(());
+        }
+        Operation::Add { name } => todo.add_task(name),
+        Operation::Complete { id } => todo.complete_task(id)?,
+        Operation::Delete { id } => {
+            todo.delete_task(id)?;
+        }
+    }
+    todo.save()?;
+
+    Ok(())
 }
